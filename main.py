5e420f74a2ff6269423f4b39f7009e340a10c955
Wenceslas-Angelo <wenceslasangelo@gmail.com>
2024-05-16 21:44:44 +0300

    background image

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pygame
 
 pygame.init()
-
+bgImage = pygame.image.load("./assets/bg.jpg")
 pygame.display.set_caption("Shooter Game")
-pygame.display.set_mode((1080, 620))
+screen = pygame.display.set_mode((1080, 620))
 
 running = True
 
 while running:
+    screen.blit(bgImage, (0, -300))
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
