cb04bb35dfd5e0a28a362289f98906935cd87c17
Wenceslas-Angelo <wenceslasangelo@gmail.com>
2024-05-17 15:24:20 +0300

    rotate projectile

--- modules/Projectile.py
+++ modules/Projectile.py
@@ -6,20 +6,20 @@ class Projectile(pygame.sprite.Sprite):
     def __init__(self, player):
         super().__init__()
         self.player = player
-        self.velocity = 5
+        self.velocity = 2
         self.image = pygame.image.load("./assets/projectile.png")
-        self.rect = self.image.get_rect()
         self.image = pygame.transform.scale(self.image, (50, 50))
+        self.origin_image = self.image.copy()
+        self.rect = self.image.get_rect()
         self.rect.x = player.rect.x + 120
         self.rect.y = player.rect.y + 80
-        self.origin_image = self.image
         self.angle = 0
 
     def remove(self):
         self.player.all_projectile.remove(self)
 
     def rotate(self):
-        self.angle += 12
+        self.angle += 5
         self.image = pygame.transform.rotozoom(self.origin_image, self.angle, 1)
         self.rect = self.image.get_rect(center=self.rect.center)
 
