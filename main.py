a8383320dc60857937f7d2ad8ea5a39fb770752d
Wenceslas-Angelo <wenceslasangelo@gmail.com>
2024-05-17 15:05:32 +0300

    create projectile

--- main.py
+++ main.py
@@ -12,18 +12,27 @@ while running:
     screen.blit(bgImage, (0, -300))
     screen.blit(game.player.image, game.player.rect)
 
+    for projectile in game.player.all_projectile:
+        projectile.move()
+
+    game.player.all_projectile.draw(screen)
+
     if game.key_pressed.get(pygame.K_RIGHT) and game.player.rect.x + game.player.rect.width < screen.get_width():
         game.player.move_right()
     elif game.key_pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
         game.player.move_left()
 
     pygame.display.flip()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
         elif event.type == pygame.KEYDOWN:
             game.key_pressed[event.key] = True
+            if event.key == pygame.K_SPACE:
+                game.player.launch_projectile()
         elif event.type == pygame.KEYUP:
             game.key_pressed[event.key] = False
+
 
--- modules/Player.py
+++ modules/Player.py
@@ -1,16 +1,22 @@
 import pygame
+from modules.Projectile import Projectile
 
 
 class Player(pygame.sprite.Sprite):
 
     def __init__(self):
+        super().__init__()
         self.health = 100
         self.max_health = 100
         self.attack = 10
         self.velocity = 5
+        self.all_projectile = pygame.sprite.Group()
         self.image = pygame.image.load("./assets/player.png");
         self.rect = self.image.get_rect()
         self.rect.y = 400
+
+    def launch_projectile(self):
+        self.all_projectile.add(Projectile(self))
 
     def move_right(self):
         self.rect.x += self.velocity
