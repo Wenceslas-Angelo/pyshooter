952d46d4496a5d44c59e1790ad5775b025e89601
Wenceslas-Angelo <wenceslasangelo@gmail.com>
2024-05-17 14:22:58 +0300

    restrict player mouvement

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ while running:
     screen.blit(bgImage, (0, -300))
     screen.blit(game.player.image, game.player.rect)
 
-    if game.key_pressed.get(pygame.K_RIGHT):
+    if game.key_pressed.get(pygame.K_RIGHT) and game.player.rect.x + game.player.rect.width < screen.get_width():
         game.player.move_right()
-    elif game.key_pressed.get(pygame.K_LEFT):
+    elif game.key_pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
         game.player.move_left()
 
     pygame.display.flip()
